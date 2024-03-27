3e5d6a0076853319f2ce7c952ed8eba7768cddb1
Abdulrahman Mokhles <aboodmokhles84@gmail.com>
2024-03-27 09:11:14 +0200

    create

--- src/app/Product/Components/add-product/add-product.component.ts
+++ src/app/Product/Components/add-product/add-product.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ProductsService } from '../../Services/products.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -13,6 +14,13 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AddProductComponent {
   constructor(private service: ProductsService, private http: HttpClient,) { }
+
+  Form1 = new FormGroup({
+    name : new FormControl("",Validators.required),
+    price : new FormControl("",Validators.required),
+    quantity : new FormControl("",Validators.required),
+    description : new FormControl("",Validators.required),
+  })
 
   public description: any = "";
 
@@ -26,14 +34,18 @@ export class AddProductComponent {
     }
   }
 
-  Add(name: any, price: any, description: any, quantity: any) {
-    // let image : any = this.imagefilename
+  // Add(name: any, price: any, description: any, quantity: any) {
+  //   // let image : any = this.imagefilename
 
-    const formdata = new FormData();
-    let image = formdata.append("image", this.imagefilename);
-    let newproduct = { name, price, description, quantity, image };
-    console.log(newproduct);
-    this.service.AddProduct(newproduct).subscribe();
+  //   const formdata = new FormData();
+  //   let image = formdata.append("image", this.imagefilename);
+  //   let newproduct = { name, price, description, quantity, image };
+  //   console.log(newproduct);
+  //   this.service.AddProduct(newproduct).subscribe();
+  // }
+
+  Add() {
+
   }
 
 }
